7ab786ea9df6c065025641b90da898b024cb74bb
Fernanda Mello <fernandam04@outlook.com>
2022-02-24 20:08:40 -0300

    Criação de classe de depósito e receitas do repositório de finanças

---
+++ src/test/java/com/learning/financas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
